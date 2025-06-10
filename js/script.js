5bf0931218d61626254534204e915cca719cd9be
default <devin.boucher@stu.ocsb.ca>
2025-06-10 16:15:38 +0000

    removed extra comment

--- js/script.js
+++ js/script.js
@@ -4,12 +4,6 @@
 // Created on: April 2025
 // This file contains the JS functions for index.html
 
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Devin Boucher
-// Created on: April 2025
-// This file contains the JS functions for index.html
-
 'use strict'
 
 /**
